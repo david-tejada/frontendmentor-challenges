aa5ac21383e9f1752ea7aaf9aa7dcd0969ddf53e
David Tejada <davidxtejada@gmail.com>
2023-09-19 11:20:54 +0200

    Multiple changes

--- 15-news-homepage/main.js
+++ 15-news-homepage/main.js
@@ -11,3 +11,49 @@ mainNavToggle.addEventListener("click", () => {
     mainNavigation.setAttribute("data-visible", false);
   }
 });
+
+const navLinks = document.querySelectorAll(".main-navigation li a");
+
+for (const link of navLinks) {
+  link.addEventListener("click", () => {
+    mainNavToggle.setAttribute("aria-expanded", false);
+    mainNavigation.setAttribute("data-visible", false);
+  });
+}
+
+mainNavToggle.addEventListener("keydown", (event) => {
+  console.log(event);
+  if (event.key === "Tab" && event.shiftKey) {
+    event.preventDefault();
+    lastNavLink.focus();
+  }
+});
+
+const lastNavLink = document.querySelector(".main-navigation li:last-child a");
+
+lastNavLink.addEventListener("keydown", (event) => {
+  if (event.key === "Tab" && !event.shiftKey) {
+    event.preventDefault();
+    mainNavToggle.focus();
+  }
+});
+
+const articles = document.querySelectorAll("article");
+
+for (const article of articles) {
+  const link = article.querySelector("a");
+  article.style.cursor = "pointer";
+
+  let down;
+  let up;
+  article.addEventListener("mousedown", () => {
+    down = +new Date();
+  });
+  article.addEventListener("mouseup", (event) => {
+    up = +new Date();
+
+    if (up - down < 200 && event.target !== link) {
+      link.click();
+    }
+  });
+}
