5384df1fb380f3a7faddb23196fc1b004c7f2d8f
David Tejada <davidxtejada@gmail.com>
2023-10-02 13:11:39 +0200

    Finish and simplify color themes

--- 17-calculator-app/tailwind.config.js
+++ 17-calculator-app/tailwind.config.js
@@ -7,25 +7,14 @@ export default {
         main: "hsl(var(--color-background-main) / <alpha-value>)",
         input: "hsl(var(--color-background-input) / <alpha-value>)",
         screen: "hsl(var(--color-background-screen) / <alpha-value>)",
-        key: {
-          DEFAULT: "hsl(var(--color-background-key) / <alpha-value>)",
-          special: "hsl(var(--color-background-key-special) / <alpha-value>)",
-          equals: "hsl(var(--color-background-key-equals) / <alpha-value>)",
-        },
+        key: "hsl(var(--color-background-key) / <alpha-value>)",
       },
       shadow: {
-        key: {
-          DEFAULT: "hsl(var(--color-shadow-key) / <alpha-value>)",
-          special: "hsl(var(--color-shadow-key-special) / <alpha-value>)",
-          equals: "hsl(var(--color-shadow-key-equals) / <alpha-value>)",
-        },
+        key: "hsl(var(--color-shadow-key) / <alpha-value>)",
       },
       content: {
         DEFAULT: "hsl(var(--color-content) / <alpha-value>)",
-        key: {
-          DEFAULT: "hsl(var(--color-content-key) / <alpha-value>)",
-          equals: "hsl(var(--color-content-key-equals) / <alpha-value>)",
-        },
+        key: "hsl(var(--color-content-key) / <alpha-value>)",
       },
       accent: "hsl(var(--color-accent) / <alpha-value>)",
       white: "hsl(0deg 0% 100%)",
