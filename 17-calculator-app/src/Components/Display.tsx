6c9799e8397ff15ef8d2dd236c0b3ec045a3e1cc
David Tejada <davidxtejada@gmail.com>
2023-10-05 16:21:59 +0200

    Add aria-live="polite" to display

--- 17-calculator-app/src/Components/Display.tsx
+++ 17-calculator-app/src/Components/Display.tsx
@@ -4,7 +4,10 @@ interface DisplayProps {
 
 export function Display({ value }: DisplayProps) {
   return (
-    <div className="my-8 grid h-24 items-center rounded-lg bg-background-screen p-6 text-right text-4xl">
+    <div
+      className="my-8 grid h-24 items-center rounded-lg bg-background-screen p-6 text-right text-4xl"
+      aria-live="polite"
+    >
       <div className="overflow-hidden">
         <p className="float-right">{value}</p>
       </div>
