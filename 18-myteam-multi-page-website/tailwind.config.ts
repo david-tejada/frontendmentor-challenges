1d49380c423427b01919136600321a39a00f69f2
David Tejada <davidxtejada@gmail.com>
2023-11-15 14:35:53 +0100

    Set up colors

--- 18-myteam-multi-page-website/tailwind.config.ts
+++ 18-myteam-multi-page-website/tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      white: "hsl(0deg 0% 100%)",
+      coral: "hsl(0deg 87% 73%)",
+      blue: "hsl(179deg 42% 63%)",
+      green: {
+        600: "hsl(186deg 98% 17%)",
+        700: "hsl(186deg 100% 14%)",
+        800: "hsl(186deg 96% 10%)",
+        900: "hsl(186deg 100% 8%)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
