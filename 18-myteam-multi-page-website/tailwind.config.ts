b020ff9215da17b235bbac2fd12082dbd1e15d00
David Tejada <davidxtejada@gmail.com>
2023-11-16 18:47:22 +0100

    Add image patterns to tailwind config

--- 18-myteam-multi-page-website/tailwind.config.ts
+++ 18-myteam-multi-page-website/tailwind.config.ts
@@ -19,6 +19,18 @@ const config: Config = {
         900: "hsl(186deg 100% 8%)",
       },
     },
+    backgroundImage: {
+      "pattern-about-1": "url('/bg-pattern-about-1-mobile-nav-1.svg')",
+      "pattern-about-2": "url('/bg-pattern-about-2-contact-1.svg')",
+      "pattern-about-4": "url('/bg-pattern-about-4.svg')",
+      "pattern-contact-2": "url('/bg-pattern-contact-2.svg')",
+      "pattern-home-1": "url('/bg-pattern-home-1.svg')",
+      "pattern-home-2": "url('/bg-pattern-home-2.svg')",
+      "pattern-home-3": "url('/bg-pattern-home-3.svg')",
+      "pattern-home-4": "url('/bg-pattern-home-4-about-3.svg')",
+      "pattern-home-5": "url('/bg-pattern-home-5.svg')",
+      "pattern-home-6": "url('/bg-pattern-home-6-about-5.svg')",
+    },
   },
   plugins: [],
 };
