bb3c52b540176b4c927604b5f2e335cb0473f7d5
David Tejada <davidxtejada@gmail.com>
2023-11-23 18:13:49 +0100

    Add section "Meet the directors" to the About page

--- 18-myteam-multi-page-website/src/app/about/page.tsx
+++ 18-myteam-multi-page-website/src/app/about/page.tsx
@@ -1,11 +1,12 @@
 import Container from "@/components/Container";
+import { PersonnelCard } from "@/components/PersonnelCard";
 
 export default function About() {
   return (
     <>
       <div className="bg-green-600 bg-pattern-about-1 bg-[position:bottom_-100px_right_-100px] bg-no-repeat pb-24 pt-4 md:bg-[position:bottom_right_-100px] lg:pt-8">
         <Container className="relative grid lg:grid-cols-[4fr_8fr]">
-          <h1 className="mx-auto max-w-[10ch] text-center text-4xl font-bold leading-10 lg:mx-0 lg:mt-4 lg:shrink-0 lg:grow-[2] lg:text-left lg:text-6xl">
+          <h1 className="mx-auto max-w-[10ch] text-center text-3xl font-bold leading-10 lg:mx-0 lg:mt-4 lg:shrink-0 lg:grow-[2] lg:text-left lg:text-4xl">
             About
           </h1>
           <p className="mx-auto mt-8 max-w-[52ch] text-center leading-7 lg:mx-0 lg:max-w-none lg:shrink lg:grow lg:text-left lg:before:absolute lg:before:top-0 lg:before:h-1 lg:before:w-12 lg:before:bg-coral">
@@ -14,6 +15,82 @@ export default function About() {
             and deliver incredible outcomes. Talented, diverse teams shape the
             best products and experiences. We’ll bring those teams to you.
           </p>
+        </Container>
+      </div>
+
+      <div className="bg-green-700 bg-[url('/bg-pattern-about-2-contact-1.svg'),url('/bg-pattern-home-4-about-3.svg')] bg-[position:top_-100px_left_-100px,bottom_right] bg-no-repeat py-20 md:bg-[position:top_left_-100px,bottom_right] md:py-24 lg:py-36">
+        <Container>
+          <h2 className="text-center text-2xl lg:text-3xl">
+            Meet the directors
+          </h2>
+
+          <div className="my-12 grid gap-[4rem_2rem] sm:grid-cols-2 lg:grid-cols-3">
+            <PersonnelCard
+              name="Nikita Marks"
+              title="Founder & CEO"
+              avatarUrl="/avatar-nikita.jpg"
+              twitterUrl="#"
+              linkedInUrl="#"
+            >
+              “It always amazes me how much talent there is in every corner of
+              the globe.”
+            </PersonnelCard>
+
+            <PersonnelCard
+              name="Cristian Duncan"
+              title="Co-founder & COO"
+              avatarUrl="/avatar-christian.jpg"
+              twitterUrl="#"
+              linkedInUrl="#"
+            >
+              “Distributed teams required unique processes. You need to approach
+              work in a new way.” Cristian Duncan
+            </PersonnelCard>
+
+            <PersonnelCard
+              name="Cruz Hamer"
+              title="Co-founder & CTO"
+              avatarUrl="/avatar-cruz.jpg"
+              twitterUrl="#"
+              linkedInUrl="#"
+            >
+              “Technology is at the forefront of enabling distributed teams.
+              That&apos;s where we come in.”
+            </PersonnelCard>
+
+            <PersonnelCard
+              name="Drake Heaton"
+              title="Business Development Lead"
+              avatarUrl="/avatar-drake.jpg"
+              twitterUrl="#"
+              linkedInUrl="#"
+            >
+              “Hiring similar people from similar backgrounds is a surefire way
+              to stunt innovation.”
+            </PersonnelCard>
+
+            <PersonnelCard
+              name="Griffin Wise"
+              title="Lead Marketing"
+              avatarUrl="/avatar-griffin.jpg"
+              twitterUrl="#"
+              linkedInUrl="#"
+            >
+              “Unique perspectives shape unique products, which is what you need
+              to survive these days.”
+            </PersonnelCard>
+
+            <PersonnelCard
+              name="Aden Allan"
+              title="Head of Talent"
+              avatarUrl="/avatar-aden.jpg"
+              twitterUrl="#"
+              linkedInUrl="#"
+            >
+              “Empowered teams create truly amazing products. Set the north star
+              and let them follow it.”
+            </PersonnelCard>
+          </div>
         </Container>
       </div>
     </>
---
+++ 18-myteam-multi-page-website/src/components/PersonnelCard.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import IconLinkedIn from "./icons/IconLinkedIn";
+import IconTwitter from "./icons/IconTwitter";
+
+type PersonnelCardProps = {
+  name: string;
+  title: string;
+  avatarUrl: string;
+  twitterUrl?: string;
+  linkedInUrl?: string;
+  children: React.ReactNode;
+};
+
+export function PersonnelCard({
+  name,
+  title,
+  avatarUrl,
+  twitterUrl,
+  linkedInUrl,
+  children,
+}: PersonnelCardProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div
+      className={`relative justify-center ${
+        isExpanded ? "bg-green-900" : "bg-green-800"
+      } pb-16 pt-8 text-center`}
+    >
+      {!isExpanded && (
+        <Image
+          src={avatarUrl}
+          width={96}
+          height={96}
+          alt=""
+          className="mx-auto mb-4 rounded-full border-2 border-white"
+        />
+      )}
+
+      <p className="font-bold text-blue">{name}</p>
+      {!isExpanded && <p className="font-medium italic">{title}</p>}
+
+      {isExpanded && (
+        <p className="mx-auto mt-4 max-w-[24ch] font-semibold">{children}</p>
+      )}
+
+      {isExpanded && (twitterUrl || linkedInUrl) && (
+        <div className="mt-6 flex justify-center gap-4">
+          {twitterUrl && (
+            <Link href="#">
+              <IconTwitter className="fill-white hover:fill-coral" />
+            </Link>
+          )}
+
+          {linkedInUrl && (
+            <Link href="#">
+              <IconLinkedIn className="fill-white hover:fill-coral" />
+            </Link>
+          )}
+        </div>
+      )}
+
+      <button
+        type="button"
+        className={`absolute bottom-[-2rem] left-[calc(50%-2rem)] grid h-16 w-16 place-items-center rounded-full ${
+          isExpanded ? "bg-blue hover:bg-coral" : "bg-coral hover:bg-blue"
+        }`}
+        onClick={() => setIsExpanded((previous) => !previous)}
+      >
+        <div className="sr-only">more info</div>
+        {(isExpanded && (
+          <Image src="/icon-close.svg" width={16} height={16} alt="" />
+        )) || <Image src="/icon-cross.svg" width={16} height={16} alt="" />}
+      </button>
+    </div>
+  );
+}
