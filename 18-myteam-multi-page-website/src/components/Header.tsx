2e8d1371a70e51dc1fc5bbea4143be1e09a0fdec
David Tejada <davidxtejada@gmail.com>
2023-11-27 09:19:11 +0100

    Avoid showing the wrong nav for an instant

--- 18-myteam-multi-page-website/src/components/Header.tsx
+++ 18-myteam-multi-page-website/src/components/Header.tsx
@@ -8,11 +8,13 @@ import Container from "./Container";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState<boolean | undefined>(false);
+  const [navReady, setNavReady] = useState(false);
   const navRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
     const mediaQueryList = window.matchMedia("(min-width: 640px)");
     if (mediaQueryList.matches) setIsOpen(undefined);
+    setNavReady(true);
 
     mediaQueryList.addEventListener("change", (mediaQueryList) => {
       if (mediaQueryList.matches) {
@@ -41,66 +43,68 @@ export default function Header() {
           <Image src="logo.svg" alt="" width="160" height="40" />
         </Link>
 
-        <nav
-          className="flex grow justify-end"
-          ref={navRef}
-          onBlur={(event) => {
-            if (
-              isOpen &&
-              event.relatedTarget &&
-              !navRef.current?.contains(event.relatedTarget)
-            ) {
-              setIsOpen(false);
-            }
-          }}
-        >
-          {isOpen !== undefined && (
-            <HamburgerButton
-              expanded={isOpen}
-              onClick={() => {
-                setIsOpen((previous) => !previous);
-              }}
-            />
-          )}
-
-          <ul
-            className={`${
-              isOpen
-                ? "translate-x-0 bg-pattern-about-1 bg-[bottom_right_-100px] bg-no-repeat"
-                : ""
-            } ${
-              isOpen === false ? "translate-x-full" : ""
-            } fixed bottom-0 right-0 top-0 bg-green-600 p-16 pt-40 transition-transform sm:static sm:flex sm:grow sm:items-center sm:gap-8 sm:p-0`}
+        {navReady && (
+          <nav
+            className="flex grow justify-end"
+            ref={navRef}
+            onBlur={(event) => {
+              if (
+                isOpen &&
+                event.relatedTarget &&
+                !navRef.current?.contains(event.relatedTarget)
+              ) {
+                setIsOpen(false);
+              }
+            }}
           >
-            <li>
-              <Link
-                href="/"
-                className="hover:text-coral"
-                onClick={() => setIsOpen(isOpen && false)}
-              >
-                home
-              </Link>
-            </li>
-            <li className="mt-8 sm:mt-0">
-              <Link
-                href="/about"
-                className="hover:text-coral"
-                onClick={() => setIsOpen(isOpen && false)}
-              >
-                about
-              </Link>
-            </li>
-            <li className="mt-10 sm:ml-auto sm:mt-0">
-              <Link
-                href="/contact"
-                className="rounded-full border px-6 py-2 hover:bg-white hover:text-green-600"
-                onClick={() => setIsOpen(isOpen && false)}
-              >
-                contact us
-              </Link>
-            </li>
-          </ul>
-        </nav>
+            {isOpen !== undefined && (
+              <HamburgerButton
+                expanded={isOpen}
+                onClick={() => {
+                  setIsOpen((previous) => !previous);
+                }}
+              />
+            )}
+
+            <ul
+              className={`${
+                isOpen
+                  ? "translate-x-0 bg-pattern-about-1 bg-[bottom_right_-100px] bg-no-repeat"
+                  : ""
+              } ${
+                isOpen === false ? "translate-x-full" : ""
+              } fixed bottom-0 right-0 top-0 bg-green-600 p-16 pt-40 transition-transform sm:static sm:flex sm:grow sm:items-center sm:gap-8 sm:p-0`}
+            >
+              <li>
+                <Link
+                  href="/"
+                  className="hover:text-coral"
+                  onClick={() => setIsOpen(isOpen && false)}
+                >
+                  home
+                </Link>
+              </li>
+              <li className="mt-8 sm:mt-0">
+                <Link
+                  href="/about"
+                  className="hover:text-coral"
+                  onClick={() => setIsOpen(isOpen && false)}
+                >
+                  about
+                </Link>
+              </li>
+              <li className="mt-10 sm:ml-auto sm:mt-0">
+                <Link
+                  href="/contact"
+                  className="rounded-full border px-6 py-2 hover:bg-white hover:text-green-600"
+                  onClick={() => setIsOpen(isOpen && false)}
+                >
+                  contact us
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        )}
       </Container>
     </header>
   );
