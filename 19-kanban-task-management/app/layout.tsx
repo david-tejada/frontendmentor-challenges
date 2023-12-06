4c32352a8a4f6d74a8ad98cd0452154a20214148
David Tejada <davidxtejada@gmail.com>
2023-12-06 11:12:00 +0100

    Initial setup

---
+++ 19-kanban-task-management/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor | Kanban task management web app",
+  description: "Frontend Mentor | Kanban task management web app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={plusJakartaSans.className}>{children}</body>
+    </html>
+  );
+}
---
+++ 19-kanban-task-management/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <p className="bg-red-500 p-4 font-thin">Hello world</p>
+    </main>
+  );
+}
